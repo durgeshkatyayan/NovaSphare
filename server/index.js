dce057ebcd1a932715b193b5e2a946527c692763
Durgesh Mishra <srjdurgeshflutter@gmail.com>
2026-07-18 00:47:25 +0530

    new serve

--- server/index.js
+++ server/index.js
@@ -9,22 +9,17 @@ require('./database');
 const configPassport = require('./passport/config');
 const routes = require('./routes/index');
 
-<<<<<<< HEAD
 const assetFolder = path.resolve(__dirname, '../dist/');
 const port = Number(process.env.PORT || 3101);
-=======
-// const assetFolder = path.resolve(__dirname, '../dist/');
-const port = process.env.PORT || 3000;
->>>>>>> 9abd9c04f08e7eaae43f39a52da66af68238e304
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static(assetFolder));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
-app.use(bodyParser.json());
-
 configPassport(app, express);
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
