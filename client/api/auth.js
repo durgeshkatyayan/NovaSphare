1f1556c94b692190d272f54aa108021d9f47aad3
Durgesh Mishra <srjdurgeshflutter@gmail.com>
2026-07-18 15:15:21 +0530

    hh

--- client/api/auth.js
+++ client/api/auth.js
@@ -3,18 +3,18 @@ import request from 'superagent';
 import { handleError, handleSuccess } from '_utils/api';
 
 export const postRegister = user =>
-  request.post('https://your-render-app.onrender.com/api/auth/register')
+  request.post('https://novasphare.onrender.com/api/auth/register')
     .send(user)
     .then(handleSuccess)
     .catch(handleError);
 
 export const postLogin = user =>
-  request.post('https://your-render-app.onrender.com/api/auth/login')
+  request.post('https://novasphare.onrender.com/api/auth/login')
     .send(user)
     .then(handleSuccess)
     .catch(handleError);
 
 export const postLogout = () =>
-  request.post('https://your-render-app.onrender.com/api/auth/logout')
+  request.post('https://novasphare.onrender.com/api/auth/logout')
     .then(handleSuccess)
     .catch(handleError);
